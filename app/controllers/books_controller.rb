400d91b557d6d812dadcca2b5db9609bf6ef696d
Chikennannbann <kamsk517@gmail.com>
2022-04-17 06:38:56 +0000

    add destroy+

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(id:current_user)
+    redirect_to book_path(@book)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   get "/homes/about" => "homes#about", as: "about"
 
 end
