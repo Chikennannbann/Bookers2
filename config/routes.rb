1f89f987c143f3f1ccbaa0b0a76016e97b3b003e
Chikennannbann <kamsk517@gmail.com>
2022-04-17 10:56:50 +0000

    add edit-profile, layouts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
-  get "/homes/about" => "homes#about", as: "about"
+  get "home/about" => "homes#about", as: "about"
 
 end
